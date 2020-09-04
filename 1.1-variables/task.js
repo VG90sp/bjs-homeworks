efc4c5111e02ea104d3a81a991c2272fb4e256b9
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-09-04 11:31:49 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,27 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let averageNumber = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return averageNumber;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+    let y = 22;
+    let z = 0;
+    let result = x * y + 5 * z + x - 1;
+    return result;
 }
