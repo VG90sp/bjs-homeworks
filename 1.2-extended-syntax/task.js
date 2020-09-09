f7991d87668045ffa6d682a7375b55fd4201ed65
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-09-09 11:35:09 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,32 @@
+"use strict"
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sum = 0;
+    let arrayMark = marks;
+
+    if (marks.length == 0) {
+        return 0;
+    }else  {
+        console.log ("среднее число, первых пяти оценок");
+        arrayMark = marks.slice(0, 5);
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sum += arrayMark[i]; 
+    }
+      let averageMark = sum / arrayMark.length;
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+
+  if (age >= 18) {
+    return `Не желаете ли олд-фэшн, ${name}?`;
+  } else {
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  }
 }
