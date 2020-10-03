cbbb90c96cb9d3e4b76c6661681c82df12e58961
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-10-03 14:10:52 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,33 @@
 //String.prototype.isPalindrome - для задачи №1
-
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function isPalindrome() {
+    let sentence = this.toUpperCase();
+    for (let i = 0; i <= sentence.length; i++) {
+      if (sentence[i] === sentence[sentence.length - 1 - i]) {
+        return true;
+      } return false;
+    }
 }
 
+function getAverageMark(marks)  {
+    if (marks.length === 0){
+        return 0
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++ ){
+        sum = sum + marks[i];
+    }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
+}
+
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let diff = now - Number(new Date(birthday));
+    let age = diff / 31557600000;
+    if (age > 18) {
+        return true;
+    } else false;
+
 }
